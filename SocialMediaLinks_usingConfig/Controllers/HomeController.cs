d90e9f99c1ba9c60c65b838f91c0bce2c3dfeb2b
jmark <jaymark.carba@cit.edu>
2024-01-13 16:26:32 +0800

    Add Options Pattern to get Configuration

---
+++ SocialMediaLinks_usingConfig/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using SocialMediaLinks_usingConfig.OptionsPattern;
+
+namespace SocialMediaLinks_usingConfig.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IConfiguration _configuration;
+        public HomeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return Json(_configuration.GetSection("Social_Media").Get<SocialMediaOptions>());
+        }
+    }
+}
---
+++ SocialMediaLinks_usingConfig/OptionsPattern/SocialMediaOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace SocialMediaLinks_usingConfig.OptionsPattern
+{
+    public class SocialMediaOptions
+    {
+        public string Facebook {  get; set; }
+        public string Gmail { get; set; }
+        public string Twitter { get; set; }
+    }
+}
